bd442198681b8ef8bafeb69f1fd23699deb4e448
Evan <973295056@qq.com>
2022-11-24 09:52:01 +0800

    images

--- Grad_CAM/main_cnn.py
+++ Grad_CAM/main_cnn.py
@@ -40,7 +40,7 @@ def main(args):
     input_tensor = torch.unsqueeze(img_tensor, dim=0)
 
     cam = GradCAM(model=model, target_layers=target_layers, use_cuda=False)
-    target_category = None # 281  # tabby, tabby cat
+    target_category = 412 # 281  # tabby, tabby cat
     # target_category = 254  # pug, pug-dog
 
     grayscale_cam = cam(input_tensor=input_tensor, target_category=target_category)
@@ -55,7 +55,7 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_classes', type=int, default=2)
+    parser.add_argument('--num_classes', type=int, default=1000)
     parser.add_argument('--device', default='cuda:0', help='device id (i.e. 0 or 0,1 or cpu)')
     parser.add_argument('--weights', type=str, default='../weights/pre_efficientnetv2-s.pth',
                         help='initial weights path')
