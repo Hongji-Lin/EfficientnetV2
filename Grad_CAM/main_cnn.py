d1a6e3681e817507065d1afe742191829b79d0f9
Hongji-Lin <973295056@qq.com>
2022-11-24 15:24:04 +0800

    111

--- Grad_CAM/main_cnn.py
+++ Grad_CAM/main_cnn.py
@@ -11,17 +11,31 @@ import argparse
 
 
 def main(args):
-    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
-    model = create_model(num_classes=args.num_classes)
-    if args.weights != "":
-        if os.path.exists(args.weights):
-            weights_dict = torch.load(args.weights, map_location=device)
-            load_weights_dict = {k: v for k, v in weights_dict.items()
-                                 if model.state_dict()[k].numel() == v.numel()}
-            print(model.load_state_dict(load_weights_dict, strict=False))
-        else:
-            raise FileNotFoundError("not found weights file: {}".format(args.weights))
+    # device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+    # model = create_model(num_classes=args.num_classes)
+    # if args.weights != "":
+    #     if os.path.exists(args.weights):
+    #         weights_dict = torch.load(args.weights, map_location=device)
+    #         load_weights_dict = {k: v for k, v in weights_dict.items()
+    #                              if model.state_dict()[k].numel() == v.numel()}
+    #         print(model.load_state_dict(load_weights_dict, strict=False))
+    #     else:
+    #         raise FileNotFoundError("not found weights file: {}".format(args.weights))
+    # target_layers = [model.blocks[-1]]
 
+    # model = create_model(num_classes=args.num_classes)
+    # model.load_state_dict(torch.load("../weights/pre_efficientnetv2-s.pth"))
+    # model = model.to(device)
+    # model.eval()
+    # target_layers = [model.blocks[-1]]
+
+    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+    # create model
+    model = create_model(num_classes=1000).to(device)
+    # load model weights
+    model_weight_path = "../weights/pre_efficientnetv2-s.pth"
+    model.load_state_dict(torch.load(model_weight_path, map_location=device), strict=False)
+    model.eval()
     target_layers = [model.blocks[-1]]
 
     data_transform = transforms.Compose([transforms.ToTensor(),
@@ -40,7 +54,7 @@ def main(args):
     input_tensor = torch.unsqueeze(img_tensor, dim=0)
 
     cam = GradCAM(model=model, target_layers=target_layers, use_cuda=False)
-    target_category = 412 # 281  # tabby, tabby cat
+    target_category = 1 # 281  # tabby, tabby cat
     # target_category = 254  # pug, pug-dog
 
     grayscale_cam = cam(input_tensor=input_tensor, target_category=target_category)
@@ -55,7 +69,7 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_classes', type=int, default=1000)
+    parser.add_argument('--num_classes', type=int, default=2)
     parser.add_argument('--device', default='cuda:0', help='device id (i.e. 0 or 0,1 or cpu)')
     parser.add_argument('--weights', type=str, default='../weights/pre_efficientnetv2-s.pth',
                         help='initial weights path')
