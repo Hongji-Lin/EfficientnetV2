5470f0edf61d80935f1c96148804ceb33c0e80b1
Hongji-Lin <973295056@qq.com>
2022-11-28 09:18:01 +0800

    epoch=10

--- unet/Unet_train.py
+++ unet/Unet_train.py
@@ -147,7 +147,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--num_classes', type=int, default=2)
-    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--batch-size', type=int, default=1)
     parser.add_argument('--lr', type=float, default=0.01)
     parser.add_argument('--lrf', type=float, default=0.01)
