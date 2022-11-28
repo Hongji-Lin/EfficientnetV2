ce6d85a5a3485a3442cc2431c2c35f63782d36d2
Hongji-Lin <973295056@qq.com>
2022-11-28 15:53:38 +0800

    1

--- unet/src/unet.py
+++ unet/src/unet.py
@@ -98,7 +98,7 @@ class UNet(nn.Module):
         x = self.up2(x, x3)
         x = self.up3(x, x2)
         x = self.up4(x, x1)
-        x = torch.flatten(x)
+        x = torch.flatten(x, 1)
         x = self.classifier(x)
 
         return x
